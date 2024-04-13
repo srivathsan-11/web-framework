93f04b37458bf501a5bb74268ae1a9a8bd593204
srivathsan-11 <165238134+srivathsan-11@users.noreply.github.com>
2024-04-13 09:58:19 +0530

    Add files via upload

---
+++ webframe work day 4/Web Frameworks/demo/src/main/java/com/project/demo/dao/Student.java
@@ -0,0 +1,14 @@
+package com.project.demo.dao;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+}
